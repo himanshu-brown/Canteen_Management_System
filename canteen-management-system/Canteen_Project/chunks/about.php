2a3e968ef5eb7e8b31fe7bf2663de860f5c5dfd7
Himanshu Sharma <54981812+himanshu-brown@users.noreply.github.com>
2022-01-08 23:10:09 +0530

    Add files via upload

---
+++ canteen-management-system/Canteen_Project/chunks/about.php
@@ -0,0 +1,8 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">If you are hungry and need some refreshments after a long routine class, then try our delecious food. You can order online food inside your campus and you can get your food delivered in your classroom or any place inside the campus.</p>
+				<a href="/RestroGirls/about-restro-girls.php" class="waves-effect waves-light btn" style="background: #ee6e73 !important;">Read More &raquo;</a>
+		      </div>
+	</section>
---
+++ canteen-management-system/Canteen_Project/chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Your College Canteen</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>GOOD FOOD GOOD LIFE GOOD STUDY</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
